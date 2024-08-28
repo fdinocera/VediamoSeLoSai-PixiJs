de1211502b1530a7da71ffa43269013a5bd25366
fdinocera <fdincoera@libero.it>
2024-08-29 01:37:04 +0200

    modified:   js/vite.config.js

--- js/vite.config.js
+++ js/vite.config.js
@@ -1,4 +1,32 @@
+// export default defineConfig({
+//   esbuild: {
+//     supported: {
+//       'top-level-await': true
+//     },
+//   },
+//   optimizeDeps: {
+//     include: ['pixi.js'],
+//   },
+//   base: './', // Imposta il percorso base per tutte le risorse 
+//               //altre configurazioni
+
+// });
+
+
+
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
 export default defineConfig({
+  root: './', // Indica la cartella root del progetto
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   esbuild: {
     supported: {
       'top-level-await': true
@@ -7,4 +35,4 @@ export default defineConfig({
   optimizeDeps: {
     include: ['pixi.js'],
   },
-});
+})
